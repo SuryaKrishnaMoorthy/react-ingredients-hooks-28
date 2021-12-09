99339d118d3a268a91a6ac2214fa0e2405a93689
Surya Krishna Moorthy <37917817+SuryaKrishnaMoorthy@users.noreply.github.com>
2021-12-09 16:52:46 -0500

    add error modal component and styling

--- src/components/UI/ErrorModal.js
+++ src/components/UI/ErrorModal.js
@@ -1,9 +1,21 @@
-const ErrorModal = () => {
-	return (
-		<div>
-			
-		</div> 
-	)
+import React, { Fragment } from "react";
+import './ErrorModal.css';
+
+const ErrorModal = (props) => {
+  return (
+    <Fragment>
+      <div className="backdrop" onClick={props.onClose} />
+      <div className="error-modal">
+				<h2>An Error Occurred!</h2>
+				<p>{props.children}</p>
+				<div>
+					<button type="button" onClick={props.onClose}>
+						Okay
+					</button> 
+				</div> 
+			</div>
+    </Fragment>
+  );
 };
 
 export default ErrorModal;
